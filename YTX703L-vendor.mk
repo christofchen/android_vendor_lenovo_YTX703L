e27df781a3a604ba659472bbdcf1fc1994366818
Vladimir Oltean <olteanv@gmail.com>
2017-08-25 23:55:00 +0300

    more lib dependencies for ims (volte). some need shims.
    Signed-off-by: Vladimir Oltean <olteanv@gmail.com>

--- YTX703L-vendor.mk
+++ YTX703L-vendor.mk
@@ -86,11 +86,15 @@ PRODUCT_COPY_FILES += \
     vendor/lenovo/YTX703L/proprietary/etc/init/thermal-engine.rc:system/etc/init/thermal-engine.rc
 
 # ims + dependencies
+# IMS == IP Media Subsystem (VoLTE)
 PRODUCT_COPY_FILES += \
     vendor/lenovo/YTX703L/proprietary/bin/imscmservice:system/bin/imscmservice \
     vendor/lenovo/YTX703L/proprietary/bin/imsdatadaemon:system/bin/imsdatadaemon \
     vendor/lenovo/YTX703L/proprietary/bin/imsqmidaemon:system/bin/imsqmidaemon \
     vendor/lenovo/YTX703L/proprietary/bin/ims_rtp_daemon:system/bin/ims_rtp_daemon \
+    vendor/lenovo/YTX703L/proprietary/vendor/lib64/librcc.so:system/vendor/lib64/librcc.so \
+    vendor/lenovo/YTX703L/proprietary/vendor/lib64/libimsmedia_jni.so:system/vendor/lib64/libimsmedia_jni.so \
+    vendor/lenovo/YTX703L/proprietary/vendor/lib64/lib-imscamera.so:system/vendor/lib64/lib-imscamera.so \
     vendor/lenovo/YTX703L/proprietary/vendor/lib64/lib-imsrcscmservice.so:system/vendor/lib64/lib-imsrcscmservice.so \
     vendor/lenovo/YTX703L/proprietary/vendor/lib64/lib-imsrcscm.so:system/vendor/lib64/lib-imsrcscm.so \
     vendor/lenovo/YTX703L/proprietary/vendor/lib64/lib-imsrcscmclient.so:system/vendor/lib64/lib-imsrcscmclient.so \
